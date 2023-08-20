38711d53817c33e812185fd604be85165b311c3e
Anil Gehlot <119098076+Anil-Gehlot@users.noreply.github.com>
2023-08-20 16:01:42 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Number of occurrence/number_of_occurrence.py
@@ -0,0 +1,12 @@
+#User function Template for python3
+class Solution:
+
+	def count(self,arr, n, x):
+		# code here
+		count = 0
+		for i in arr:
+		    if i==x:
+		        count +=1
+		
+		return count
+
